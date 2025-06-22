152eb7e3c4596166a129b263788768dadd5fffd5
tyrant369 <prthbdhr@gmail.com>
2025-06-22 21:35:21 +0530

    msgQ

---
+++ msgQ/src/main/java/projects/parthib/MsgQ.java
@@ -0,0 +1,11 @@
+package projects.parthib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgQ {
+    public static void main(String[] args) {
+        SpringApplication.run(MsgQ.class, args);
+    }
+}
